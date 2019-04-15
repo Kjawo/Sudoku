4124398b255219eb1fd26106b7d87c4585f740a5
Kjawo <konjaworski@gmail.com>
2019-04-15 08:41:08 +0200

    Correct styling

--- src/main/java/FieldArray.java
+++ src/main/java/FieldArray.java
@@ -1,11 +1,9 @@
+import java.util.List;
+
 import org.apache.commons.lang3.builder.EqualsBuilder;
 import org.apache.commons.lang3.builder.HashCodeBuilder;
 import org.apache.commons.lang3.builder.ToStringBuilder;
 import org.apache.commons.lang3.builder.ToStringStyle;
-
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
 
 class FieldArray {
 
--- src/main/java/SudokuBoard.java
+++ src/main/java/SudokuBoard.java
@@ -1,10 +1,10 @@
+import java.util.Arrays;
+import java.util.List;
+
 import org.apache.commons.lang3.builder.EqualsBuilder;
 import org.apache.commons.lang3.builder.HashCodeBuilder;
 import org.apache.commons.lang3.builder.ToStringBuilder;
 import org.apache.commons.lang3.builder.ToStringStyle;
-
-import java.util.Arrays;
-import java.util.List;
 
 public class SudokuBoard {
 
